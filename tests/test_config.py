09441e266a685bfe6478cda21ab5ccdff626823f
2blrm2 <2blrm2@gmail.com>
2021-07-25 01:32:44 +0530

    webapp  added

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,4 +1,17 @@
-def test_genric():
+def test_something():  #for test case name must start with test
     a=2
     b=2
     assert a==b
+
+import pytest
+
+class NotInRange(Exception):
+    def __init__(self,message="Value not in Range"):
+        self.message = message
+        super().__init__(self.message)
+
+def test_genric():
+    a=5
+    with pytest.raises(NotInRange):
+        if a not in range(10,15):
+            raise NotInRange
