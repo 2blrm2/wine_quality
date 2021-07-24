5e0620f6399ee62b8b276154d6d9107277046a9b
2blrm2 <2blrm2@gmail.com>
2021-07-25 00:01:52 +0530

    pytest created

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    description= "its our own package for wine quality project",
+    author = "Balram Chaudhary",
+    packages = find_packages(),
+    license = "MIT"
+)
